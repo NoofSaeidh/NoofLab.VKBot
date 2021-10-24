9738d181ac55907c0df9b87d7449fd142ba0fae9
Alexey Kuznetsov <akuznetsov@acumatica.com>
2021-10-25 00:06:27 +0300

    Service and options

---
+++ NoofLab.VKBot.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NoofLab.VKBot.Core;
+using NoofLab.VKBot.Core.Configuration;
+
+namespace NoofLab.VKBot.Console
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            return host.RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args);
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddHostedService<VkService>();
+                services.AddSingleton<Config>(provider =>
+                    provider
+                        .GetService<IConfiguration>()
+                        .GetSection("Config")
+                        .Get<Config>());
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ NoofLab.VKBot.Core/VkService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NoofLab.VKBot.Core.Configuration;
+
+namespace NoofLab.VKBot.Core
+{
+    public class VkService : IHostedService
+    {
+        private readonly Config _config;
+
+        public VkService(Config config)
+        {
+            _config = config;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
